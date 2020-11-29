0aa3d72679a42abadd6e9ad97fa12e9b7a621861
Jang Gyu Hyun <jgh950829@gmail.com>
2020-11-29 23:00:09 +0900

    아이템 습득 및 수류탄 장착시 이펙트 추가

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -9,7 +9,19 @@ public class Player : MonoBehaviour
     public float speed;
     public GameObject[] weapons;
     public bool[] hasWeapons;
+    public GameObject[] grenades;
+    public int hasGrenades;
     
+    
+    public int ammo;
+    public int coin;
+    public int health;
+
+    public int maxAmmo;
+    public int maxCoin;
+    public int maxHealth;
+    public int maxHasGrenades;
+
     float hAxis;
     float vAxis;
     
@@ -190,6 +202,37 @@ public class Player : MonoBehaviour
         }
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Item")
+        {
+            Item item = other.GetComponent<Item>();
+            switch(item.type){
+                case Item.Type.Ammo:
+                    ammo += item.value;
+                    if(ammo > maxAmmo)
+                        ammo = maxAmmo;
+                    break;
+                case Item.Type.Coin:
+                    coin += item.value;
+                    if(coin > maxCoin)
+                        coin = maxCoin;
+                    break;
+                case Item.Type.Heart:
+                    health += item.value;
+                    if(health > maxHealth)
+                        health = maxHealth;
+                    break;
+                case Item.Type.Grenade:
+                grenades[hasGrenades].SetActive(true);
+                    hasGrenades += item.value;
+                    if(hasGrenades > maxHasGrenades)
+                        hasGrenades = maxHasGrenades;
+                    break;
+            }
+            Destroy(other.gameObject);
+        }
+    }
      void OnTriggerStay(Collider other)
     {
         if (other.tag == "Weapon")
